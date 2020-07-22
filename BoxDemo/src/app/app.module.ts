9a8140dfa139e02d0ed65b6b41fb72bd1a7058e4
11LoneWolf11 <47557736+11LoneWolf11@users.noreply.github.com>
2020-07-22 11:28:49 +0100

    Update app.module.ts
    Now it will work without any error because import { Ng2GoogleRecaptchaModule } from 'ng2-google-recaptcha'; has been commented out.

--- BoxDemo/src/app/app.module.ts
+++ BoxDemo/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './boxLogin/login.component';
 //import { RecaptchaModule } from 'ng-recaptcha';
-import { Ng2GoogleRecaptchaModule } from 'ng2-google-recaptcha';
+//import { Ng2GoogleRecaptchaModule } from 'ng2-google-recaptcha';
 //import * as Rx from 'rxjs';
 //import 'rxjs';
-import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, Ng2GoogleRecaptchaModule],
